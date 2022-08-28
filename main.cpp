09db472bd60874533d43b73975fef61c6795a7cc
BernardoOlisan <46250121+BernardoOlisan@users.noreply.github.com>
2022-08-28 17:04:37 -0500

    modifying input layer and passing the neuron value of each

--- main.cpp
+++ main.cpp
@@ -5,38 +5,30 @@
 
 #include "model.h"
 
-float speed = 0.0002f;
-float x_speed = 0.002f;
-float angle = 0.0f;
-
-float rocket_top = -0.95f;
-float rocket_bottom = -0.85f;
-float rocket_x = 0.05f;
+RocketNN Rocket;
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
-
+    
     if (key == GLFW_KEY_A && action == GLFW_REPEAT || key == GLFW_KEY_A && action == GLFW_PRESS) {
-        angle -= 5.0f; 
+        Rocket.angle -= 5.0; 
         a = GL_TRIANGLE_FAN;
     } else {
         a = GL_LINE_STRIP;
     }
 
     if (key == GLFW_KEY_D && action == GLFW_REPEAT || key == GLFW_KEY_D && action == GLFW_PRESS) {
-        angle += 5.0f;
+        Rocket.angle += 5.0;
         d = GL_TRIANGLE_FAN;
     } else {
         d = GL_LINE_STRIP;
     }
 }
 
-
 int main(void) {
     GLFWwindow* window;
-    RocketNN Rocket;
 
     if (!glfwInit())
         return -1;
@@ -50,6 +42,7 @@ int main(void) {
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
 
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(.5,.7,.8,.5);
@@ -57,47 +50,7 @@ int main(void) {
         atmosphere();
         keys();
 
-        float rotation = angle*1/360;
-        float top_movement = speed - rotation * speed;
-        float side_movement = rotation * x_speed;
-        
-        if (std::fabs(rotation) > 0.25f) {
-            Rocket.rocket(
-                rocket_top -= top_movement, 
-                rocket_bottom -= top_movement, 
-                rocket_x += side_movement, 
-                255,0,0
-            );
-        } else {
-            Rocket.rocket(
-                rocket_top += top_movement, 
-                rocket_bottom += top_movement, 
-                rocket_x += side_movement, 
-                255,0,0
-            );
-        }
-
-        if (rocket_bottom >= std::cos(random_num/41-0.5)-0.2 && 
-            rocket_x-0.03 <= random_num/41-0.5 && 
-            rocket_x+0.03 >= random_num/41-0.5) {
-
-            std::cout << "Good!" << std::endl;
-            rocket_top = -0.95f; 
-            rocket_bottom = -0.85f; 
-            angle = 0.0f;
-            random_num = random_number(); 
-            
-        } else if (rocket_bottom >= std::cos(rocket_x)-0.2 || 
-            rocket_x <= -1.0 || rocket_x >= 1.0 || 
-            rocket_bottom <= -1.0) {
-
-            std::cout << "Bad" << std::endl;
-            rocket_top = -0.95f; 
-            rocket_bottom = -0.85f;  
-            rocket_x = 0.05f; 
-            angle = 0.0f;
-            random_num = random_number();
-        }
+        Rocket.rocket(255, 255, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- model.h
+++ model.h
@@ -1,9 +1,9 @@
 #include "GL/glfw3.h"
+#include <algorithm>
 #include <cstdlib>
 #include <cmath>
 #include <time.h>
-
-extern float angle;
+#include <vector>
 
 char a = GL_LINE_STRIP;
 char d = GL_LINE_STRIP;
@@ -32,7 +32,7 @@ void atmosphere() {
             glVertex3f(random_num/41-0.5, std::cos(random_num/41-0.5)-0.2, 0); 
         glEnd();
 
-        glColor3f(1,1,1);
+        glColor3f(1, 1, 1);
     glPopMatrix();
 }
 
@@ -62,47 +62,125 @@ void keys() {
 
 class RocketNN {
 public:
-    int input = 0;
-    
-    void rocket(
-            float rocket_top, 
-            float rocket_bottom, 
-            float rocket_x, 
-            int red, 
-            int green, 
-            int blue
-        ) {
+    float speed = 0.0002;
+    float x_speed = 0.002;
+
+    float rocket_top = -0.85;
+    float rocket_bottom = -0.95;
+    float rocket_x = 0.00;
+    float angle = 0.0;
+
+    // Input neurons (multidimentional vectors)
+    // {{neurons value}, {neurons connected to}, {weights values}}
+    std::vector<std::vector<float>> wall_north = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> wall_east = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> wall_south = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> wall_west = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> rocket_x_ = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> rocket_y = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> rocket_rotation = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> point_x = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> point_y = {{0}, {0}, {0}};
+    std::vector<std::vector<float>> point_distance = {{0}, {0}, {0}};
+
+    // Output neurons
+    std::vector<float> left = {0};
+    std::vector<float> right = {0};
+
+    // score and Genetic Encoding Scheme
+    float score = 0.0;
+
+    void rocket(int red, int green, int blue) {
+        // Movements
+        float rotation = angle*1/360;
+        float top_movement = speed - rotation * speed;
+        float side_movement = rotation * x_speed;
+
+        if (std::fabs(rotation) > 0.25) {
+            rocket_top -= top_movement;
+            rocket_bottom -= top_movement; 
+            rocket_x += side_movement;
+        } else {
+            rocket_top += top_movement;
+            rocket_bottom += top_movement; 
+            rocket_x += side_movement;
+        }
+        
+        // Collisions
+        if (rocket_top >= std::cos(random_num/41-0.5)-0.2 && 
+            rocket_x-0.03 <= random_num/41-0.5 && 
+            rocket_x+0.03 >= random_num/41-0.5) {
+            std::cout << "Good!" << std::endl;
+            rocket_top = -0.85; 
+            rocket_bottom = -0.95; 
+            rocket_x = 0.00; 
+            angle = 0.0;
+            random_num = random_number(); 
+        } else if (rocket_top >= std::cos(rocket_x) - 0.2 || 
+                   rocket_x <= -1.0 || rocket_x >= 1.0 || 
+                   rocket_top <= -1.0) {
+            std::cout << "Bad" << std::endl;
+            rocket_top = -0.85; 
+            rocket_bottom = -0.95;  
+            rocket_x = 0.00; 
+            angle = 0.0;
+            random_num = random_number();
+        }
+
+        // Inputs neurons values
+        wall_north[0][0] = 1-((std::cos(rocket_x) - 0.2) - rocket_top);
+        wall_east[0][0] = rocket_x;
+        wall_south[0][0] = 0.15 - rocket_top;
+        wall_west[0][0] = rocket_x * -1;
+
+        rocket_x_[0][0] = rocket_x;
+        rocket_y[0][0] = rocket_top - 0.05;
+        rocket_rotation[0][0] = rotation;
+
+        point_x[0][0] = random_num/41-0.5;
+        point_y[0][0] = std::cos(random_num/41-0.5) - 0.2;
+        
+        float x1 = rocket_x;
+        float x2 = point_x[0][0];
+        float y1 = rocket_top;
+        float y2 = point_y[0][0];
+        point_distance[0][0] = std::sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+
+        // Rocket Graphics
         glPushMatrix();
 
             glColor3ub(red, green, blue);
-            glTranslatef(0+rocket_x, 0+rocket_top, 0);
-                glRotatef(angle, 0, 0, -1.0f);
-            glTranslatef(0, 0-rocket_top, 0);
+            glTranslatef(0+rocket_x, 0+rocket_bottom, 0);
+                glRotatef(angle, 0, 0, -1.0);
+            glTranslatef(0, 0-rocket_bottom, 0);
             glBegin(GL_TRIANGLE_FAN);
-                glVertex2f(0.02f, rocket_top);
-                glVertex2f(0.02f, rocket_bottom);
-                glVertex2f(-0.02f, rocket_bottom);
-                glVertex2f(-0.02f, rocket_top);
+                glVertex2f(0.02, rocket_top);
+                glVertex2f(0.02, rocket_bottom);
+                glVertex2f(-0.02, rocket_bottom);
+                glVertex2f(-0.02, rocket_top);
             glEnd();
 
             glColor3f(0,0,0);
             glLineWidth(1.2);
             glBegin(GL_LINE_STRIP);
-                glVertex2f(0.02f, rocket_top);
-                glVertex2f(-0.02f, rocket_top);
-                glVertex2f(-0.02f, rocket_bottom);
-                glVertex2f(0.02f, rocket_bottom);
-                glVertex2f(0.02f, rocket_top);
+                glVertex2f(0.02, rocket_top);
+                glVertex2f(-0.02, rocket_top);
+                glVertex2f(-0.02, rocket_bottom);
+                glVertex2f(0.02, rocket_bottom);
+                glVertex2f(0.02, rocket_top);
             glEnd();
 
             glColor3f(1,1,1);
         glPopMatrix();
-    }
+    } 
 };
 
-// 1. Simple mode for non-ai player with colission and everything, wall colission
-// 2. Have AI NEAT mode, each rocket is going to be a nn, so change every
-//thing so that the class can have the speed and controls of the rocket and
-//in the main function you can just pass the class and the rocket creation,
-//change the color or set it randomly so that multiple (20) rockets can evolve 
-// 3. clean code 
+//DONE 1. Pass the needed data to the input layers (environment sensors) all in -1 to 1 "1 means closest except in point_distance", relu in hidden will delete negative numbers
+
+// 2. Connect with vector structure all the input and output layers (at the end there's going to be only one connection that activates sth) 
+
+// 3. Another window to preview the NN
+
+// 4. Make the evolve process 
+
+// 5. clean code
