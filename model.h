96804dbc9e183c1628171e1ac93d4b1bc84255a8
BernardoOlisan <46250121+BernardoOlisan@users.noreply.github.com>
2022-09-01 16:50:11 -0500

    adding Neat Encoding Scheme with dynamic approach & preview graphics for nn

--- model.h
+++ model.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <time.h>
 #include <vector>
+#include <string>
 
 char a = GL_LINE_STRIP;
 char d = GL_LINE_STRIP;
@@ -73,20 +74,156 @@ public:
 
     float score = 0.0;
 
-    // Input neurons (multidimentional vectors)
-    // {{neurons value}, {neurons connected to}, {weights values}}
-    std::vector<std::vector<float>> wall_north = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> wall_east = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> wall_south = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> wall_west = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> rocket_rotation = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> point_x = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> point_y = {{0}, {0}, {0}};
-    std::vector<std::vector<float>> point_distance = {{0}, {0}, {0}};
+    // Neat Encoding Scheme "Genotype"
+    // Nodes
+    std::vector<std::vector<std::vector<float>>> encoding_scheme_nodes = {
+        { // Input layer
+            {1, 0}, // {wall_north, value} 
+            {2, 0}, // {wall_east, value}
+            {3, 0}, // {wall_south, value}
+            {4, 0}, // {wall_west, value}
+            {5, 0}, // {rocket_rotation, value}
+            {6, 0}, // {point_x, value}
+            {7, 0}, // {point_y, value}
+            {8, 0}, // {point_distance, value}
+        }, 
+        { // Output layer
+            {9, 0}, // {left, value}
+            {10, 0} // {right, value}
+        }
+    };
 
-    // Output neurons
-    std::vector<float> left = {0};
-    std::vector<float> right = {0};
+    // Connections
+    std::vector<std::vector<float>> encoding_scheme_connections = {
+        { // {In, Out, Out position neuron, Weight value, enable-disable(1-0), innovation number}
+            1, 
+            9,
+            0, 
+            0.1, 
+            std::max(float(0), float(0.1/std::fabs(0.1))), 
+            1,
+        }, 
+        {
+            1,
+            10,
+            0,
+            0.2,
+            std::max(float(0), float(0.2/std::fabs(0.2))), 
+            2,
+        },
+        {
+            2, 
+            9,
+            0,
+            0.1,
+            std::max(float(0), float(0.1/std::fabs(0.1))), 
+            3,
+        },
+        {
+            2,
+            10,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            4
+        },
+        {
+            3,
+            9,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            5
+        },
+        {
+            3,
+            10,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            6
+        },
+        {
+            4,
+            9,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            7
+        },
+        {
+            4,
+            10,
+            0,
+            0.4,
+            std::max(float(0), float(0.4/std::fabs(0.4))), 
+            8
+        },
+        {
+            5,
+            9,
+            0,
+            0.1,
+            std::max(float(0), float(0.1/std::fabs(0.1))), 
+            9
+        },
+        {
+            5,
+            10,
+            0,
+            0.1,
+            std::max(float(0), float(0.1/std::fabs(0.1))), 
+            10
+        },
+        {
+            6,
+            9,
+            0,
+            0.2,
+            std::max(float(0), float(0.2/std::fabs(0.2))), 
+            11
+        },
+        {
+            6,
+            10,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            12
+        },
+        {
+            7,
+            9,
+            0,
+            0.3,
+            std::max(float(0), float(0.3/std::fabs(0.3))), 
+            13
+        },
+        {
+            7,
+            10,
+            0,
+            0.1,
+            std::max(float(0), float(0.1/std::fabs(0.1))), 
+            14
+        },
+        {
+            8,
+            9,
+            0,
+            -0.3,
+            std::max(float(0), float(-0.3/std::fabs(-0.3))), 
+            15
+        },
+        {
+            8,
+            10,
+            0,
+            0.5,
+            std::max(float(0), float(0.5/std::fabs(0.5))), 
+            16
+        },
+    };
 
 
     void rocket(int red, int green, int blue) {
@@ -127,21 +264,21 @@ public:
         }
 
         // Values for input neurons
-        wall_north[0][0] = 1-((std::cos(rocket_x) - 0.2) - rocket_top);
-        wall_east[0][0] = rocket_x;
-        wall_south[0][0] = 0.15 - rocket_top;
-        wall_west[0][0] = 0 - rocket_x;
+        encoding_scheme_nodes[0][0][1] = 1-((std::cos(rocket_x) - 0.2) - rocket_top);
+        encoding_scheme_nodes[0][1][1] = rocket_x;
+        encoding_scheme_nodes[0][2][1] = 0.15 - rocket_top;
+        encoding_scheme_nodes[0][3][1] = 0 - rocket_x;
 
-        rocket_rotation[0][0] = rotation;
+        encoding_scheme_nodes[0][4][1] = rotation;
 
-        point_x[0][0] = random_num/41-0.5;
-        point_y[0][0] = std::cos(random_num/41-0.5) - 0.2;
+        encoding_scheme_nodes[0][5][1] = random_num/41-0.5;
+        encoding_scheme_nodes[0][6][1] = std::cos(random_num/41-0.5) - 0.2;
         
         float x1 = rocket_x;
-        float x2 = point_x[0][0];
+        float x2 = encoding_scheme_nodes[0][5][1];
         float y1 = rocket_top;
-        float y2 = point_y[0][0];
-        point_distance[0][0] = (1 - std::sqrt(pow(x2-x1, 2) + pow(y2-y1, 2)));
+        float y2 = encoding_scheme_nodes[0][6][1];
+        encoding_scheme_nodes[0][7][1] = (1 - std::sqrt(pow(x2-x1, 2) + pow(y2-y1, 2)));
 
         // Rocket Graphics
         glPushMatrix();
@@ -173,20 +310,8 @@ public:
 
 
     void draw_nn() {
-        // Input layer vector in "reverse" to get the correct order
-        std::vector<float> input_layer = {
-            point_distance[0][0], 
-            point_y[0][0],
-            point_x[0][0], 
-            rocket_rotation[0][0],
-            wall_west[0][0], 
-            wall_south[0][0],
-            wall_east[0][0], 
-            wall_north[0][0]
-        };
-
         // Input layer (neurons)
-        for (int i = 0; i < input_layer.size(); i++) {
+        for (int i = 0; i < encoding_scheme_nodes[0].size(); i++) {
             glColor3f(1, 1, 1);
             glBegin(GL_LINE_LOOP);
                 for(int f = 0; f < 20; f++)
@@ -200,7 +325,7 @@ public:
                 }
             glEnd();
 
-            glColor4f(1, 1, 1, input_layer[i]);
+            glColor4f(1, 1, 1, encoding_scheme_nodes[0][i][1]);
             glBegin(GL_POLYGON);
                 for(int f = 0; f < 20; f++)
                 {
@@ -213,10 +338,34 @@ public:
                 }
             glEnd();
             glColor3f(1, 1, 1);
+
+            // Synapsis
+            for (int f = 0; f < encoding_scheme_connections.size(); f++) {
+                if (encoding_scheme_connections[f][0] == i+1) {
+                    for (int j = 0; j < encoding_scheme_nodes[1].size(); j++) {
+                        if (encoding_scheme_connections[f][1] == encoding_scheme_nodes[1][j][0]) {
+                            glPushMatrix();
+                                glBegin(GL_LINES);
+                                    glVertex2f(-0.74, (float(i) * float(0.8) / 5) - 0.5);
+                                    glVertex2f(0.6, encoding_scheme_connections[f][2]); 
+                                glEnd();
+                            glPopMatrix();
+                        }
+                    }
+                }
+            }
         } 
 
         // Output layer (neurons)
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < encoding_scheme_nodes[1].size(); i++) {
+            for (int f = 0; f < encoding_scheme_connections.size(); f++) {
+                for (int j = 0; j < encoding_scheme_nodes[1].size(); j++) {
+                    if (encoding_scheme_connections[f][1] == encoding_scheme_nodes[1][j][0]) {
+                        encoding_scheme_connections[f][2] = (float(j) * float(0.8) / 5) + 0.01;
+                    }
+                }
+            }
+
             glBegin(GL_POLYGON);
                 for(int f = 0; f < 20; f++)
                 {
@@ -232,12 +381,17 @@ public:
     }
 };
 
-//DONE 1. Pass the needed data to the input layers (environment sensors) all in -1 to 1 "1 means closest except in point_distance", relu in hidden will delete negative numbers
 
-// 2. Connect with vector structure all the input and output layers (at the end there's going to be only one connection that activates sth) 
+/* 
+2. Connect with vector structure all the input and output layers (at the end there's going to be only one connection that activates sth) 
+2.1 Once everything is connected, do the weighted sum in the while loop and let it run, without learning
+2.2 Extend the dynamic vector for hidden neurons, control everything with the arrays
 
-// 3. Another window to preview the NN
+3. Another window to preview the NN
 
-// 4. Make the evolve process 
+4. Make the evolve process 
 
-// 5. clean code
+5. clean code
+
+6. optimized it
+*/
